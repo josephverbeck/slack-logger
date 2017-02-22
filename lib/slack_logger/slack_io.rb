b17d1c4588ff882bf3f3aa98b8e866133982a402
josephverbeck <joseph.verbeck@nih.gov>
2017-02-22 10:23:31 -0500

    fixing bug where message was being sent to slack even if message was blank

--- lib/slack_logger/slack_io.rb
+++ lib/slack_logger/slack_io.rb
@@ -6,28 +6,30 @@ module SlackLogger
     end
 
     def self.write(message)
-      client = SlackLogger.client
-      client.auth_test
-      client.chat_postMessage({
-                                  channel: SlackLogger.channel,
-                                  attachments: [{
-                                                    pretext: message.to_s,
-                                                    ts: "#{Time.now.utc.to_i}",
-                                                    fields: [
-                                                        {
-                                                            title: "Project",
-                                                            value: "#{Rails.application.class.parent_name}",
-                                                            short: true
-                                                        },
-                                                        {
-                                                            title: "Environment",
-                                                            value: "#{Rails.env}",
-                                                            short: true
-                                                        }
-                                                    ],
-                                                    color: "#F35A00"
-                                                }].to_json,
-                                  as_user: true})
+      unless message.blank?
+        client = SlackLogger.client
+        client.auth_test
+        client.chat_postMessage({
+                                    channel: SlackLogger.channel,
+                                    attachments: [{
+                                                      pretext: message.to_s,
+                                                      ts: "#{Time.now.utc.to_i}",
+                                                      fields: [
+                                                          {
+                                                              title: "Project",
+                                                              value: "#{Rails.application.class.parent_name}",
+                                                              short: true
+                                                          },
+                                                          {
+                                                              title: "Environment",
+                                                              value: "#{Rails.env}",
+                                                              short: true
+                                                          }
+                                                      ],
+                                                      color: "#F35A00"
+                                                  }].to_json,
+                                    as_user: true})
+      end
     end
 
   end
